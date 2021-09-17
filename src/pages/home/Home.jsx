f603dfb960c3bae9867c58f32e52d945e8c4644c
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-09-17 16:22:43 +0600

    home page navbar and feature pages added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
-
+import Feature from "../../component/feature/Feature"
+import List from "../../component/list/List"
+import Navbar from "../../component/navbar/Navbar"
 import "./home.scss"
 
 const Home = () => {
     return (
         <div className="home">
-            
+            <Navbar />
+            <Feature />
+            <List />
         </div>
     )
 }
