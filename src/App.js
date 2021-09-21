da896847fe266e4d2e693b33262cd5695c0dce07
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-09-21 11:20:31 +0600

    login added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './app.scss';
 import Home from "./pages/home/Home";
-import Register from './pages/register/Register';
+// import Login from './pages/login/Login';
+// import Register from './pages/register/Register';
 
 function App() {
   return (
     <div>
-      <Register />
+      <Home />
     </div>
   );
 }
